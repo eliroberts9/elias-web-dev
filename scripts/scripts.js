2c032ff04ef38c6528d563b6b918732f9d604214
Elias Roberts <eliroberts9@gmail.com>
2018-09-05 09:21:11 -0600

    added fadeout effect to navbar and jumbotron

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,15 @@
 $(function () {
-    $('[data-toggle="popover"]').popover()
-  })
+  $('[data-toggle="popover"]').popover()
+});
 
-  $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+});
+
+$(window).scroll(function(){
+  $(".jumbotron").css("opacity", 1 - $(window).scrollTop() / 250);
+});
+
+$(window).scroll(function(){
+  $(".navbar").css("opacity", 1 - $(window).scrollTop() / 250);
+});
